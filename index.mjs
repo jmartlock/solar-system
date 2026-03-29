06b8a436aa854e9b57a831cdbae9d386346cca38
Justin Martlock <jmartlock@csumb.edu>
2026-03-29 15:15:41 -0800

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,36 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const planets = (await import('npm-solarsystem')).default;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', async(req, res) => {
+    let apiKey = "7756a1e81f817c186cf57294e1c19b37b49c54b8f34e7c499ee0ce5cd86cd16e";
+	let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&query=solar-system`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let randomImage = data.urls.full;
+    res.render("index",{"image":randomImage})
+});
+
+app.listen(3000, () => {
+   console.log('server started');
+});
+
+app.get('/planet', (req, res) => {
+ let planetName = req.query.planetName;
+ let planetInfo = planets[`get${planetName}`]();
+ res.render('planet', { planetInfo, planetName });
+});
+
+app.get('/nasa', async (req, res) => {
+    const today = new Date().toISOString().split('T')[0];
+    const apiKey = '9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD';
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${today}`;
+
+    const response = await fetch(url);
+    const apod = await response.json();
+
+    res.render('nasa', { apod });
+});
